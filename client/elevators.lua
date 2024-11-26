6dea1ba7638576dcee16f68016757a4f3c88bda6
IlMelons <melonss2002@gmail.com>
2024-11-26 23:21:03 +0100

    UPDATE

--- client/elevators.lua
+++ client/elevators.lua
@@ -4,28 +4,28 @@ Elevators = {}
 
 function ShowElevatorMenu(elevatorPlans, elevatorLabel)
     local menuOptions = {}
-    for _, planData in ipairs(elevatorPlans) do
+    for i = 1, #elevatorPlans do
         local planOption = {
-            title = planData.title,
-            description = planData.description,
-            icon = planData.locked and "fas fa-lock" or "fas fa-lock-open",
-            iconColor = planData.locked and "#FF0000" or "#008000",
+            title = elevatorPlans[i].title,
+            description = elevatorPlans[i].description,
+            icon = elevatorPlans[i].locked and "fas fa-lock" or "fas fa-lock-open",
+            iconColor = elevatorPlans[i].locked and "#FF0000" or "#008000",
             onSelect = function()
-                local hasItem = lib.callback.await("melons_mapsutility:server:HasItem", false, planData.item)
-                if planData.locked and not hasItem then return Notify(locale("notify.title"), locale("notify.need_item"), "error") end
+                local hasItem = lib.callback.await("melons_mapsutility:server:HasItem", false, elevatorPlans[i].item)
+                if elevatorPlans[i].locked and not hasItem then return Notify(locale("notify.title"), locale("notify.need_item"), "error") end
                 DoScreenFadeOut(1000)
                 Wait(2000)
-                SetEntityCoords(PlayerPedId(), planData.teleport.x, planData.teleport.y, planData.teleport.z, false, false, false, false)
-                SetEntityHeading(PlayerPedId(), planData.teleport.w)
+                SetEntityCoords(PlayerPedId(), elevatorPlans[i].teleport.x, elevatorPlans[i].teleport.y, elevatorPlans[i].teleport.z, false, false, false, false)
+                SetEntityHeading(PlayerPedId(), elevatorPlans[i].teleport.w)
                 Wait(2000)
                 DoScreenFadeIn(1000)
                 Notify(locale("notify.title"), locale("notify.reached_plan"), "success")
             end,
             metadata = {
-                {label = locale("menu.access"), value = (planData.locked and locale("menu.locked") or locale("menu.unlocked"))},
+                {label = locale("menu.access"), value = (elevatorPlans[i].locked and locale("menu.locked") or locale("menu.unlocked"))},
             },
         }
-        menuOptions[planData.title] = planOption
+        menuOptions[#menuOptions+1] = planOption
     end
 
     local menuId = "elevator_menu"
@@ -42,7 +42,7 @@ end
 
 function CreateElevator(elevatorName, elevatorData)
     local elevatorPlans = {}
-    for _, planData in ipairs(elevatorData.plans) do
+    for _, planData in pairs(elevatorData.plans) do
         local elevatorPlan = lib.zones.box({
             coords = vec3(planData.teleport.x, planData.teleport.y, planData.teleport.z),
             size = vec3(planData.size.x, planData.size.y, planData.size.z),
--- config/elevators.lua
+++ config/elevators.lua
@@ -5,7 +5,7 @@ return {
         ["tropical"] = {
             label = "Tropical Heights",
             plans = {
-                {
+                [1] = {
                     title = "Piano Terra",
                     description = "Lobby",
                     teleport = vec4(310.7614, -916.7014, 29.4758, 69.00),
@@ -13,7 +13,7 @@ return {
                     locked = false,
                     item = "",
                 },
-                {
+                [2] = {
                     title = "Piano 1",
                     description = "Bar & NightClub",
                     teleport = vec4(310.2260, -916.6540, 52.8083, 69.00),
@@ -21,11 +21,11 @@ return {
                     locked = false,
                     item = "",
                 },
-                {
+                [3] = {
                     title = "Piano 2",
                     description = "Bar Open Space",
-                    teleport = vec4(315.9350, -913.7500, 57.4113, 135.65),
-                    size = vec3(1.0, 1.0, 1.0),
+                    teleport = vec4(315.8117, -913.8285, 57.7000, 134.00),
+                    size = vec3(2.0, 2.0, 2.5),
                     locked = true,
                     item = "water",
                 },
