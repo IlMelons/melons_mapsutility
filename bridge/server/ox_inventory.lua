925b9ca41c55d274812d310855420333030d3caf
IlMelons <melonss2002@gmail.com>
2024-11-28 12:08:46 +0100

    Added Possibility to add multiple items

--- bridge/server/ox_inventory.lua
+++ bridge/server/ox_inventory.lua
@@ -3,6 +3,14 @@ if GetResourceState("ox_inventory") ~= "started" then return end
 local ox_inventory = exports.ox_inventory
 
 function GetItemCount(source, item)
-    local count = ox_inventory:Search(source, "count", item, metadata)
-    return count
+    local count = ox_inventory:Search(source, "count", item)
+    local item_count = 0
+    if type(count) == "table" then
+        for k, v in pairs(count) do
+            item_count += v
+        end
+        return item_count
+    else
+        return count
+    end
 end
