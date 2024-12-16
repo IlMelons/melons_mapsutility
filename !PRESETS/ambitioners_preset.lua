0872d8a4d39d9fb0727deda102731d584af2d0b4
IlMelons <melonss2002@gmail.com>
2024-12-16 11:26:01 +0100

    Added Preset Ambitioners Mile High Club Map

---
+++ !PRESETS/ambitioners_preset.lua
@@ -0,0 +1,200 @@
+---------------------------------------------------------------------------------
+------------------------------ PRESETS AJAXON MAPS ------------------------------
+---------------------------------------------------------------------------------
+---@diagnostic disable
+return {
+---------------------------------------------------------------------------------
+---@ELEVATORS ---------- Copy and Paste the map preset in config/elevators.lua --
+---------------------------------------------------------------------------------
+    ["milehighclub1"] = {
+        label = "Mile High Club (Hotel)",
+        debug = false,
+        plans = {
+            [1] = {
+                title = "Rooms Floor 6",
+                description = "",
+                teleport = vec4(-154.9951, -982.9111, 212.60, 160.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [2] = {
+                title = "Rooms Floor 5",
+                description = "",
+                teleport = vec4(-154.9951, -982.9111, 206.40, 160.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [3] = {
+                title = "Rooms Floor 4",
+                description = "",
+                teleport = vec4(-154.9951, -982.9111, 200.00, 160.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [4] = {
+                title = "Rooms Floor 3",
+                description = "",
+                teleport = vec4(-154.9951, -982.9111, 193.90, 160.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [5] = {
+                title = "Rooms Floor 2",
+                description = "",
+                teleport = vec4(-154.9951, -982.9111, 187.70, 160.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [6] = {
+                title = "Rooms Floor 1",
+                description = "",
+                teleport = vec4(-154.9951, -982.9111, 181.30, 160.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [7] = {
+                title = "Lobby",
+                description = "",
+                teleport = vec4(-154.9951, -982.9111, 29.60, 160.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+        },
+    },
+    ["milehighclub2"] = {
+        label = "Mile High Club",
+        debug = false,
+        plans = {
+            [1] = {
+                title = "Rooftop",
+                description = "Helipad",
+                teleport = vec4(-162.8081, -979.9983, 270.20, 339.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [2] = {
+                title = "Yusuf's Office",
+                description = "",
+                teleport = vec4(-155.9922, -985.5590, 265.00, 339.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [3] = {
+                title = "Assmosphere",
+                description = "",
+                teleport = vec4(-155.9922, -985.5590, 254.60, 339.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [4] = {
+                title = "SPA & Gym",
+                description = "",
+                teleport = vec4(-155.9922, -985.5590, 175.00, 339.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [5] = {
+                title = "Rooftop Pool 2",
+                description = "",
+                teleport = vec4(-124.3680, -974.9234, 115.60, 250.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [6] = {
+                title = "Rooftop Pool 1",
+                description = "",
+                teleport = vec4(-155.9922, -985.5590, 53.30, 250.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [7] = {
+                title = "Cigar Lounge",
+                description = "",
+                teleport = vec4(-155.9922, -985.5590, 46.00, 250.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [8] = {
+                title = "French Restaurant",
+                description = "",
+                teleport = vec4(-155.9922, -985.5590, 39.50, 250.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+            [9] = {
+                title = "Lobby",
+                description = "",
+                teleport = vec4(-155.9922, -985.5590, 29.60, 250.00),
+                size = vec3(2.3, 2.3, 2.3),
+                locked = false,
+                item = "",
+            },
+        },
+    },
+    ["milehighclub3"] = {
+        label = "Mile High Club (Garage)",
+        debug = false,
+        plans = {
+            [1] = {
+                title = "Lobby",
+                description = "",
+                teleport = vec4(-135.4880, -984.3469, 29.60, 160.00),
+                size = vec3(2.0, 2.0, 2.3),
+                locked = false,
+                item = "",
+            },
+            [2] = {
+                title = "Garage Floor -1",
+                description = "",
+                teleport = vec4(-135.4880, -984.3469, 23.30, 160.00),
+                size = vec3(2.0, 2.0, 2.3),
+                locked = false,
+                item = "",
+            },
+            [3] = {
+                title = "Garage Floor -2",
+                description = "",
+                teleport = vec4(-135.4880, -984.3469, 18.20, 160.00),
+                size = vec3(2.0, 2.0, 2.3),
+                locked = false,
+                item = "",
+            },
+            [4] = {
+                title = "Garage Floor -3",
+                description = "",
+                teleport = vec4(-135.4880, -984.3469, 13.20, 160.00),
+                size = vec3(2.0, 2.0, 2.3),
+                locked = false,
+                item = "",
+            },
+        },
+    },
+---------------------------------------------------------------------------------
+---@BLIPS ------------------ Copy and Paste the map preset in config/blips.lua --
+---------------------------------------------------------------------------------
+    ["milehighclub"] = {
+        label = "Mile High Club",
+        coords = vector3(-107.5170, -959.7780, 29.2805),
+        display = 2,
+        sprite = 475,
+        color = 70,
+        alpha = 255,
+        scale = 0.8,
+    },
+---------------------------------------------------------------------------------
+}
