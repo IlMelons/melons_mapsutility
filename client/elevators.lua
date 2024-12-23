24683d42ed524c7200c18442279b92904be25fc0
IlMelons <melonss2002@gmail.com>
2024-12-23 23:20:17 +0100

    MAJOR UPDATE: Multiple Elevators

--- client/elevators.lua
+++ client/elevators.lua
@@ -2,7 +2,7 @@ local ConfigElevators = lib.load("config.elevators")
 
 Elevators = {}
 
-function ShowElevatorMenu(elevatorPlans, elevatorLabel)
+function ShowElevatorMenu(elevatorPlans, elevatorLabel, planIndex)
     local menuOptions = {}
     for i = 1, #elevatorPlans do
         local planOption = {
@@ -15,8 +15,15 @@ function ShowElevatorMenu(elevatorPlans, elevatorLabel)
                 if elevatorPlans[i].locked and not hasItem then return Notify(locale("notify.title"), locale("notify.need_item"), "error") end
                 DoScreenFadeOut(1000)
                 Wait(2000)
-                SetEntityCoords(PlayerPedId(), elevatorPlans[i].teleport.x, elevatorPlans[i].teleport.y, elevatorPlans[i].teleport.z, false, false, false, false)
-                SetEntityHeading(PlayerPedId(), elevatorPlans[i].teleport.w)
+
+                local targetZone = elevatorPlans[i].zones[planIndex]
+                if not targetZone or not targetZone.coords then
+                    lib.print.debug("Zone index: "..tostring(planIndex).." not found or invalid. Using zone index 1 as fallback.")
+                    targetZone = elevatorPlans[i].zones[1]
+                end
+
+                SetEntityCoords(PlayerPedId(), targetZone.coords.x, targetZone.coords.y, targetZone.coords.z, false, false, false, false)
+                SetEntityHeading(PlayerPedId(), targetZone.coords.w)
                 Wait(2000)
                 DoScreenFadeIn(1000)
                 Notify(locale("notify.title"), locale("notify.reached_plan"), "success")
@@ -43,24 +50,28 @@ end
 function CreateElevator(elevatorName, elevatorData)
     local elevatorPlans = {}
     for _, planData in pairs(elevatorData.plans) do
-        local elevatorPlan = lib.zones.box({
-            coords = vec3(planData.teleport.x, planData.teleport.y, planData.teleport.z),
-            size = vec3(planData.size.x, planData.size.y, planData.size.z),
-            rotation = planData.teleport.w,
-            onEnter = function()
-                lib.showTextUI(ConfigElevators.TextUI, {position = "left-center", icon = "fa-solid fa-building"})
-            end,
-            onExit = function()
-                lib.hideTextUI()
-            end,
-            inside = function()
-                if IsControlJustReleased(0, 38) then
-                    ShowElevatorMenu(elevatorData.plans, elevatorData.label)
-                end
-            end,
-            debug = ConfigElevators.Elevators[elevatorName].debug,
-        })
-        elevatorPlans[planData.title] = elevatorPlan
+        local zones = {}
+        for zoneIndex, zoneData in pairs(planData.zones) do
+            local elevatorZone = lib.zones.box({
+                coords = vec3(zoneData.coords.x, zoneData.coords.y, zoneData.coords.z),
+                size = zoneData.size,
+                rotation = zoneData.coords.w,
+                onEnter = function()
+                    lib.showTextUI(ConfigElevators.TextUI, {position = "left-center", icon = "fa-solid fa-building"})
+                end,
+                onExit = function()
+                    lib.hideTextUI()
+                end,
+                inside = function()
+                    if IsControlJustReleased(0, 38) then
+                        ShowElevatorMenu(elevatorData.plans, elevatorData.label, zoneIndex)
+                    end
+                end,
+                debug = ConfigElevators.Elevators[elevatorName].debug,
+            })
+            zones[zoneIndex] = elevatorZone
+        end
+        elevatorPlans[planData.title] = zones
     end
     Elevators[elevatorName] = elevatorPlans
 end
--- config/elevators.lua
+++ config/elevators.lua
@@ -8,24 +8,27 @@ return {
                 [1] = {
                     title = "Ground Floor",
                     description = "Entrance and Desk",
-                    teleport = vec4(136.0807, -761.7469, 46.00, 160.00),
-                    size = vec3(2.0, 2.0, 2.5),
+                    zones = {
+                        [1] = {coords = vec4(136.0807, -761.7469, 46.00, 160.00), size = vec3(2.0, 2.0, 2.5)}
+                    },
                     locked = false,
                     item = "",
                 },
                 [2] = {
                     title = "Floor 47",
                     description = "Legal Affairs",
-                    teleport = vec4(136.0939, -761.8068, 234.50, 160.00),
-                    size = vec3(2.0, 2.0, 2.5),
+                    zones = {
+                        [1] = {coords = vec4(136.0939, -761.8068, 234.50, 160.00), size = vec3(2.0, 2.0, 2.5)}
+                    },
                     locked = false,
                     item = "",
                 },
                 [3] = {
                     title = "Floor 49",
                     description = "Communications",
-                    teleport = vec4(136.0755, -761.7881, 242.50, 160.00),
-                    size = vec3(2.0, 2.0, 2.5),
+                    zones = {
+                        [1] = {coords = vec4(136.0755, -761.7881, 242.50, 160.00), size = vec3(2.0, 2.0, 2.5)}
+                    },
                     locked = false,
                     item = "",
                 }
