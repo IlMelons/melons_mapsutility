4cdd65acf56ccbe338dbab60c070de24835b0bba
IlMelons <melonss2002@gmail.com>
2024-12-24 13:42:28 +0100

    Updated TStudio Maps Presets for New Version

--- !PRESETS/tstudio_preset.lua
+++ !PRESETS/tstudio_preset.lua
@@ -13,24 +13,27 @@ return {
             [1] = {
                 title = "Floor 2",
                 description = "Bar Open Space",
-                teleport = vec4(315.8117, -913.8285, 57.7000, 134.00),
-                size = vec3(2.3, 2.3, 2.5),
+                zones = {
+                    [1] = {coords = vec4(315.8117, -913.8285, 57.70, 134.00), size = vec3(2.3, 2.3, 2.5)},
+                },
                 locked = false,
                 item = "",
             },
             [2] = {
                 title = "Floor 1",
                 description = "Bar & Pool",
-                teleport = vec4(310.7336, -916.7942, 53.10, 70.00),
-                size = vec3(2.3, 2.3, 2.5),
+                zones = {
+                    [1] = {coords = vec4(310.7336, -916.7942, 53.10, 70.00), size = vec3(2.3, 2.3, 2.5)},
+                },
                 locked = false,
                 item = "",
             },
             [3] = {
                 title = "Ground Floor",
                 description = "Lobby",
-                teleport = vec4(310.7352, -916.7856, 29.80, 70.00),
-                size = vec3(2.3, 2.3, 2.5),
+                zones = {
+                    [1] = {coords = vec4(310.7352, -916.7856, 29.80, 70.00), size = vec3(2.3, 2.3, 2.5)},
+                },
                 locked = false,
                 item = "",
             },
