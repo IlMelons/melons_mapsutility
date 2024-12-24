f3116b797f9b6eef6d2965aec62eee664859608e
IlMelons <melonss2002@gmail.com>
2024-12-24 10:43:04 +0100

    Updated Ajaxon Maps Presets for new version

--- !PRESETS/ajaxon_preset.lua
+++ !PRESETS/ajaxon_preset.lua
@@ -6,93 +6,57 @@ return {
 ---------------------------------------------------------------------------------
 ---@ELEVATORS ---------- Copy and Paste the map preset in config/elevators.lua --
 ---------------------------------------------------------------------------------
-    ["aldorehospital1"] = {
-        label = "Aldore Hospital 1",
+    ["aldorehospital"] = {
+        label = "Aldore Hospital",
         debug = false,
         plans = {
             [1] = {
                 title = "Rooftop",
                 description = "Garden & Helipad",
-                teleport = vec4(-468.5999, -1024.2511, 38.50, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
+                zones = {
+                    [1] = {coords = vec4(-468.5999, -1024.2511, 38.50, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                    [2] = {coords = vec4(-468.6347, -1027.4954, 38.50, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                },
                 locked = false,
                 item = "",
             },
             [2] = {
                 title = "Floor 2",
                 description = "Offices & Rooms",
-                teleport = vec4(-465.4464, -1025.4980, 33.90, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
+                zones = {
+                    [1] = {coords = vec4(-465.4464, -1025.4980, 33.90, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                    [2] = {coords = vec4(-465.5065, -1028.9021, 33.90, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                },
                 locked = false,
                 item = "",
             },
             [3] = {
                 title = "Floor 1",
                 description = "Rehabilitation & Rooms",
-                teleport = vec4(-465.4464, -1025.4980, 29.30, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
+                zones = {
+                    [1] = {coords = vec4(-465.4464, -1025.4980, 29.30, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                    [2] = {coords = vec4(-465.5065, -1028.9021, 29.30, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                },
                 locked = false,
                 item = "",
             },
             [4] = {
                 title = "Ground Floor",
                 description = "Store & Pharmacy & Exams Rooms",
-                teleport = vec4(-465.4464, -1025.4980, 24.50, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
+                zones = {
+                    [1] = {coords = vec4(-465.4464, -1025.4980, 24.50, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                    [2] = {coords = vec4(-465.5065, -1028.9021, 24.50, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                },
                 locked = false,
                 item = "",
             },
             [5] = {
                 title = "Floor -1",
                 description = "Garage",
-                teleport = vec4(-465.4464, -1025.4980, 19.90, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
-                locked = false,
-                item = "",
-            },
-        },
-    },
-    ["aldorehospital2"] = {
-        label = "Aldore Hospital 2",
-        debug = false,
-        plans = {
-            [1] = {
-                title = "Rooftop",
-                description = "Garden & Helipad",
-                teleport = vec4(-468.6347, -1027.4954, 38.50, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
-                locked = false,
-                item = "",
-            },
-            [2] = {
-                title = "Floor 2",
-                description = "Offices & Rooms",
-                teleport = vec4(-465.5065, -1028.9021, 33.90, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
-                locked = false,
-                item = "",
-            },
-            [3] = {
-                title = "Floor 1",
-                description = "Rehabilitation & Rooms",
-                teleport = vec4(-465.5065, -1028.9021, 29.30, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
-                locked = false,
-                item = "",
-            },
-            [4] = {
-                title = "Ground Floor",
-                description = "Store & Pharmacy & Exams Rooms",
-                teleport = vec4(-465.5065, -1028.9021, 24.50, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
-                locked = false,
-                item = "",
-            },
-            [5] = {
-                title = "Floor -1",
-                description = "Garage",
-                teleport = vec4(-465.5065, -1028.9021, 19.90, 90.00),
-                size = vec3(2.3, 2.3, 2.3),
+                zones = {
+                    [1] = {coords = vec4(-465.4464, -1025.4980, 19.90, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                    [2] = {coords = vec4(-465.5065, -1028.9021, 19.90, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                },
                 locked = false,
                 item = "",
             },
