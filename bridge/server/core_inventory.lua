d3529beefceb40ce8dd0495ab7af06bf3e2cb673
IlMelons <melonss2002@gmail.com>
2025-01-01 19:29:34 +0100

    Delete bridge/server directory

--- bridge/server/core_inventory.lua
+++
@@ -1,9 +0,0 @@
-if GetResourceState("core_inventory") ~= "started" then return end
-
-local core_inventory = exports.core_inventory
-
-function GetItemCount(source, items)
-    local count = core_inventory:getItemCount(source, items)
-
-    return count
-end
--- bridge/server/qb-inventory.lua
+++
@@ -1,9 +0,0 @@
-if GetResourceState("qb-inventory") ~= "started" then return end
-
-local qb_inventory = exports["qb-inventory"]
-
-function GetItemCount(source, items)
-    local count = qb_inventory:GetItemCount(source, items)
-
-    return count
-end
