8e01db48b5abf270fcd43539b9be7c5a7fa8ad72
IlMelons <melonss2002@gmail.com>
2025-02-09 20:51:58 +0100

    Add files via upload

---
+++ config/sit.lua
@@ -0,0 +1,13 @@
+return {
+	Models = {
+		[`prop_bench_01a`] = {
+			maxseats = 3,
+			anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+			seats = {
+				[1] = {offset = vec4(0.7, -0.05, 0.5, 180.0)},
+				[2] = {offset = vec4(0.0, -0.05, 0.5, 180.0)},
+				[3] = {offset = vec4(-0.7, -0.05, 0.5, 180.0)},
+			}
+		},
+	}
+}
