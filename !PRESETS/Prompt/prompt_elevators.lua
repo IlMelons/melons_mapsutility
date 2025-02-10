8efd2881307d8ec11798c5ba3424f29bc3c17184
IlMelons <melonss2002@gmail.com>
2025-02-10 20:56:01 +0100

    Updated Prompt Presets

---
+++ !PRESETS/Prompt/prompt_elevators.lua
@@ -0,0 +1,51 @@
+---------------------------------------------------------------------------------
+---@ELEVATORS ---------- Copy and Paste the map preset in config/elevators.lua --
+---------------------------------------------------------------------------------
+---@diagnostic disable
+
+["rockfordpolicedepartment"] = {
+    label = "Rockford Police Department",
+    debug = false,
+    plans = {
+        [1] = {
+            title = "Rooftop",
+            description = "",
+            zones = {
+                [1] = {coords = vec4(-579.8720, -130.9076, 52.30, 293.00), size = vec3(2.5, 2.5, 2.4)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+        [2] = {
+            title = "Floor 2",
+            description = "",
+            zones = {
+                [1] = {coords = vec4(-573.1172, -135.9981, 48.20, 112.50), size = vec3(2.5, 2.5, 2.4)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+        [3] = {
+            title = "Floor 1",
+            description = "Meeting Room & Patients Rooms",
+            zones = {
+                [1] = {coords = vec4(-572.9024, -135.9096, 43.10, 112.50), size = vec3(2.5, 2.5, 2.4)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+        [4] = {
+            title = "Ground Floor",
+            description = "",
+            zones = {
+                [1] = {coords = vec4(-572.9712, -129.1511, 38.70, 292.50), size = vec3(2.5, 2.5, 2.4)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+    },
+},
