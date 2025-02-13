7b0016b08c815e1aa6d0a31c969fb7eb2ea9ed34
IlMelons <melonss2002@gmail.com>
2025-02-13 19:10:35 +0100

    Reordered Presets Ajaxon

---
+++ !PRESETS/Ajaxon/ajaxon_blips.lua
@@ -0,0 +1,59 @@
+---------------------------------------------------------------------------------
+---@BLIPS ------------------ Copy and Paste the map preset in config/blips.lua --
+---------------------------------------------------------------------------------
+---@diagnostic disable
+
+["aldorehospital"] = {
+    label = "Aldore Hospital",
+    coords = vec3(-520.41, -965.55, 23.52),
+    display = 2,
+    sprite = 61,
+    color = 25,
+    alpha = 255,
+    scale = 0.8,
+},
+["bunker"] = {
+    label = "Bunker",
+    coords = vec3(-1510.91, 1993.35, 66.96),
+    display = 2,
+    sprite = 310,
+    color = 25,
+    alpha = 255,
+    scale = 0.8,
+},
+["burtonlsc"] = {
+    label = "Burton LSC",
+    coords = vec3(-363.62, -121.52, 38.70),
+    display = 2,
+    sprite = 311,
+    color = 25,
+    alpha = 255,
+    scale = 0.8,
+},
+["coffeeshop"] = {
+    label = "Coffee Shop",
+    coords = vec3(182.21, -228.48, 54.13),
+    display = 2,
+    sprite = 93,
+    color = 25,
+    alpha = 255,
+    scale = 0.8,
+},
+["koispa"] = {
+    label = "Koi Spa",
+    coords = vec3(-1042.17, -1471.97, 5.06),
+    display = 2,
+    sprite = 197,
+    color = 25,
+    alpha = 255,
+    scale = 0.8,
+},
+["musclegymnasium"] = {
+    label = "Muscle Gymnasium",
+    coords = vec3(114.76, 335.38, 112.13),
+    display = 2,
+    sprite = 311,
+    color = 25,
+    alpha = 255,
+    scale = 0.8,
+},
---
+++ !PRESETS/Ajaxon/ajaxon_elevators.lua
@@ -0,0 +1,66 @@
+---------------------------------------------------------------------------------
+---@ELEVATORS ---------- Copy and Paste the map preset in config/elevators.lua --
+---------------------------------------------------------------------------------
+---@diagnostic disable
+
+["aldorehospital"] = {
+    label = "Aldore Hospital",
+    debug = false,
+    plans = {
+        [1] = {
+            title = "Rooftop",
+            description = "Garden & Helipad",
+            zones = {
+                [1] = {coords = vec4(-468.5999, -1024.2511, 38.50, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                [2] = {coords = vec4(-468.6347, -1027.4954, 38.50, 90.00), size = vec3(2.3, 2.3, 2.3)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+        [2] = {
+            title = "Floor 2",
+            description = "Offices & Rooms",
+            zones = {
+                [1] = {coords = vec4(-465.4464, -1025.4980, 33.90, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                [2] = {coords = vec4(-465.5065, -1028.9021, 33.90, 90.00), size = vec3(2.3, 2.3, 2.3)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+        [3] = {
+            title = "Floor 1",
+            description = "Rehabilitation & Rooms",
+            zones = {
+                [1] = {coords = vec4(-465.4464, -1025.4980, 29.30, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                [2] = {coords = vec4(-465.5065, -1028.9021, 29.30, 90.00), size = vec3(2.3, 2.3, 2.3)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+        [4] = {
+            title = "Ground Floor",
+            description = "Store & Pharmacy & Exams Rooms",
+            zones = {
+                [1] = {coords = vec4(-465.4464, -1025.4980, 24.50, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                [2] = {coords = vec4(-465.5065, -1028.9021, 24.50, 90.00), size = vec3(2.3, 2.3, 2.3)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+        [5] = {
+            title = "Floor -1",
+            description = "Garage",
+            zones = {
+                [1] = {coords = vec4(-465.4464, -1025.4980, 19.90, 90.00), size = vec3(2.3, 2.3, 2.3)},
+                [2] = {coords = vec4(-465.5065, -1028.9021, 19.90, 90.00), size = vec3(2.3, 2.3, 2.3)},
+            },
+            locked = false,
+            item = false,
+            jobs = false,
+        },
+    },
+},
