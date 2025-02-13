bcf14bb5a6de1a4a66bdeb117dfb7eab36d0b3d1
IlMelons <melonss2002@gmail.com>
2025-02-13 19:21:51 +0100

    Reordered Presets K4mb1

---
+++ !PRESETS/K4mb1/k4mb1_blips.lua
@@ -0,0 +1,14 @@
+---------------------------------------------------------------------------------
+---@BLIPS ------------------ Copy and Paste the map preset in config/blips.lua --
+---------------------------------------------------------------------------------
+---@diagnostic disable
+
+["diamondcasino"] = {
+    label = "Diamond Casino",
+    coords = vec3(924.25, 47.39, 81.00),
+    display = 2,
+    sprite = 679,
+    color = 0,
+    alpha = 255,
+    scale = 0.8,
+},
