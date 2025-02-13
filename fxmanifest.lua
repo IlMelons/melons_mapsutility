76289d22f4ca3b19a0cffa8cb14fcac5646465cc
IlMelons <melonss2002@gmail.com>
2025-02-13 23:26:12 +0100

    Bump manifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 "yes"
 name "melons_mapsutility"
 description "An optimized resource for Blips and Elevators management"
 author "IlMelons"
-version "1.9.0"
+version "1.9.1"
 repository "https://github.com/IlMelons/melons_mapsutility"
 
 ox_lib "locale"
