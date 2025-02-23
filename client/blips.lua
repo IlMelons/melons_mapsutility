f8132ae1b5bb70aec2c93b569ff24cf2f034ccf1
IlMelons <melonss2002@gmail.com>
2025-02-23 19:45:46 +0100

    Improved code and added two exports for ext management from future scripts

--- client/blips.lua
+++ client/blips.lua
@@ -2,13 +2,37 @@ Blips = {}
 local ConfigBlips = lib.load("config.blips")
 
 function RemoveBlips()
-    for _, blip in ipairs(Blips) do
+    for _, blip in pairs(Blips) do
         RemoveBlip(blip)
     end
     Blips = {}
 end
 
-local function CreateBlip(data)
+local function DeleteBlip(blipID)
+    RemoveBlip(Blips[blipID])
+    Blips[blipID] = nil
+end
+exports("DeleteBlip", DeleteBlip)
+
+local function DeleteMultiBlips(blipID)
+    local num = 1
+    local toRemove = {}
+
+    while true do
+        local currentID = ("%s%d"):format(blipID, num)
+        if not Blips[currentID] then break end
+
+        toRemove[num] = currentID
+        num += 1
+    end
+
+    for _, name in ipairs(toRemove) do
+        DeleteBlip(name)
+    end
+end
+exports("DeleteMultiBlips", DeleteMultiBlips)
+
+local function CreateBlip(blipID, data)
     local blip = AddBlipForCoord(data.coords.x, data.coords.y, data.coords.z)
     SetBlipAlpha(blip, data.alpha or 255)
     SetBlipSprite(blip, data.sprite or 1)
@@ -19,13 +43,15 @@ local function CreateBlip(data)
     EndTextCommandSetBlipName(blip)
     SetBlipDisplay(blip, data.display or 2)
     SetBlipScale(blip, data.scale or 1.0)
-    Blips[#Blips + 1] = blip
+    Blips[blipID] = blip
 end
 exports("CreateBlip", CreateBlip)
 
-local function CreateMultiBlips(data)
+local function CreateMultiBlips(blipID, data)
+    local num = 1
     for _, point in ipairs(data.points) do
-        CreateBlip({
+        local notDuplicatedID = ("%s%d"):format(blipID, num)
+        CreateBlip(notDuplicatedID, {
             coords = point,
             alpha = data.alpha,
             sprite = data.sprite,
@@ -34,16 +60,17 @@ local function CreateMultiBlips(data)
             display = data.display,
             scale = data.scale,
         })
+        num += 1
     end
 end
 exports("CreateMultiBlips", CreateMultiBlips)
 
 function CreateBlips()
-    for _, blipData in pairs(ConfigBlips.Blips) do
+    for blipID, blipData in pairs(ConfigBlips.Blips) do
         if blipData.coords then
-            CreateBlip(blipData)
+            CreateBlip(blipID, blipData)
         elseif blipData.points then
-            CreateMultiBlips(blipData)
+            CreateMultiBlips(blipID, blipData)
         end
     end
 end
