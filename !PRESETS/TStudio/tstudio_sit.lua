4730368e596ce8c8b6fd345efe381db9e69af928
IlMelons <melonss2002@gmail.com>
2025-03-24 12:00:16 +0100

    Added New TStudio Seats Preset (Al Dente's)

--- !PRESETS/TStudio/tstudio_sit.lua
+++ !PRESETS/TStudio/tstudio_sit.lua
@@ -1,7 +1,81 @@
 ---------------------------------------------------------------------------------
 ---@SIT ---------------------- Copy and Paste the map preset in config/sit.lua --
 ---------------------------------------------------------------------------------
----@diagnostic disable
+---@diagnostic disable: exp-in-action, unknown-symbol
+
+---@description AL DENTE'S
+[`johanni_aldentes_asset_barstool`] = {
+	maxseats = 1,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.0, 0.0, 0.9, 270.0)},
+	},
+},
+[`johanni_aldentes_asset_barstool_ext`] = {
+	maxseats = 1,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.0, 0.0, 0.9, 270.0)},
+	},
+},
+[`johanni_aldentes_asset_chair_ext`] = {
+	maxseats = 1,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.0, 0.1, 0.5, 180.0)},
+	},
+},
+[`johanni_aldentes_asset_chair01`] = {
+	maxseats = 1,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.0, -0.2, 0.0, 180.0)},
+	},
+},
+[`johanni_aldentes_asset_chair02`] = {
+	maxseats = 1,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.0, 0.0, 0.6, 180.0)},
+	},
+},
+[`johanni_aldentes_asset_chair03`] = {
+	maxseats = 1,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.0, 0.1, 0.5, 180.0)},
+	},
+},
+[-61947709] = {
+	maxseats = 1,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.0, 0.0, 0.6, 180.0)},
+	},
+},
+[`johanni_aldentes_asset_seating01_ext`] = {
+	maxseats = 1,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.15, 0.10, 0.1, 270.0)},
+	},
+},
+[-694881216] = {
+	maxseats = 2,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.4, 0.0, 0.5, 180.0)},
+		[2] = {offset = vec4(-0.4, 0.0, 0.5, 180.0)},
+	},
+},
+[`johanni_aldentes_asset_seating02_ext`] = {
+	maxseats = 2,
+	anim = {scenario = "PROP_HUMAN_SEAT_BENCH"},
+	seats = {
+		[1] = {offset = vec4(0.4, 0.2, 0.1, 0.0)},
+		[2] = {offset = vec4(-0.4, 0.2, 0.1, 0.0)},
+	},
+},
 
 ---@description KEBAB KING DINER
 [-1281587804] = {
