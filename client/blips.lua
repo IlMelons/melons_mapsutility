7c4136a1d4b2f6ec0f5fd6c0af28c84903755ca5
IlMelons <melonss2002@gmail.com>
2025-04-15 10:42:13 +0200

    Updated blips feature with new improvements

--- client/blips.lua
+++ client/blips.lua
@@ -1,12 +1,5 @@
-Blips = {}
-local ConfigBlips = lib.load("config.blips")
-
-function RemoveBlips()
-    for _, blip in pairs(Blips) do
-        RemoveBlip(blip)
-    end
-    Blips = {}
-end
+local Config = lib.load("config.blips")
+local Blips = {}
 
 local function DeleteBlip(blipID)
     RemoveBlip(Blips[blipID])
@@ -14,35 +7,21 @@ local function DeleteBlip(blipID)
 end
 exports("DeleteBlip", DeleteBlip)
 
-local function DeleteMultiBlips(blipID)
-    local num = 1
-    local toRemove = {}
-
-    while true do
-        local currentID = ("%s%d"):format(blipID, num)
-        if not Blips[currentID] then break end
-
-        toRemove[num] = currentID
-        num += 1
-    end
-
-    for _, name in ipairs(toRemove) do
-        DeleteBlip(name)
-    end
-end
-exports("DeleteMultiBlips", DeleteMultiBlips)
-
 local function CreateBlip(blipID, data)
     local blip = AddBlipForCoord(data.coords.x, data.coords.y, data.coords.z)
     SetBlipAlpha(blip, data.alpha or 255)
     SetBlipSprite(blip, data.sprite or 1)
     SetBlipColour(blip, data.color or 0)
     SetBlipAsShortRange(blip, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString(data.label or "Blip")
-    EndTextCommandSetBlipName(blip)
     SetBlipDisplay(blip, data.display or 2)
     SetBlipScale(blip, data.scale or 1.0)
+
+    if data.label then
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(data.label)
+        EndTextCommandSetBlipName(blip)
+    end
+
     Blips[blipID] = blip
 end
 exports("CreateBlip", CreateBlip)
@@ -63,10 +42,9 @@ local function CreateMultiBlips(blipID, data)
         num += 1
     end
 end
-exports("CreateMultiBlips", CreateMultiBlips)
 
-function CreateBlips()
-    for blipID, blipData in pairs(ConfigBlips.Blips) do
+local function CreateBlips()
+    for blipID, blipData in pairs(Config) do
         if blipData.coords then
             CreateBlip(blipID, blipData)
         elseif blipData.points then
@@ -76,6 +54,5 @@ function CreateBlips()
 end
 
 function SetupBlips()
-    RemoveBlips()
     CreateBlips()
 end
